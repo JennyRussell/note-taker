8b333dfaf3410307967fb2696aa1ce0da726f783
Jenny Russell <moosyoolka@gmail.com>
2021-04-21 10:45:20 -0700

    deleted develop folder, changes file structure and dependencies

--- router.js
+++ router.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const database = require('./Develop/db/db.json');
+const database = require('./db/db.json');
 const fs = require('fs');
 const router = express.Router();
 
+
 // route to notes
 
-router.get('/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
     res.send(JSON.stringify(database));
     res.header("Content-Type", 'application/json');
 })
+
+router.post('/api/notes', (req, res) => {
+
+    const newNote = req.body;
+
+    fs.appendFile('db.json', newNote, (err) => {
+        if (err) throw err;
+        console.log('New note added!');
+    });
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var PORT = process.env.PORT || 3000;
 // link to assets
 app.use(express.static('public'));
 
+// use /api
+app.use('/api', router);
+
 // set express app to handle parsing of data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,5 +24,5 @@ app.listen(3000, function() {
 });
 
 // link to html pages
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
