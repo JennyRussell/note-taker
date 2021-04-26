3b174ceb0830c18b1c9bc812b3de154f29a0ccbf
Jenny Russell <moosyoolka@gmail.com>
2021-04-26 09:40:46 -0700

    added DELETE route

--- router.js
+++ router.js
@@ -8,13 +8,14 @@ const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 
 
-// route to notes
+// route to get notes for file
 router.get('/notes', function(req, res) {
     return note.allNotes().then(data => res.json(data))
         // console.log(res);
 
 });
 
+// route to post new notes
 router.post('/notes', (req, res) => {
     console.log(req.body);
     const title = req.body.title;
@@ -22,16 +23,16 @@ router.post('/notes', (req, res) => {
 
     const newNote = { title, text, id: uuidv4() };
 
-    var parsedNotes;
+    let parsedNotes;
 
     fs.readFile('db/db.json', "utf8").then(
         (notes) => {
             parsedNotes = [].concat(JSON.parse(notes))
-            var updatedNotes = [...parsedNotes, newNote];
+            let updatedNotes = [...parsedNotes, newNote];
             fs.writeFile('db/db.json', JSON.stringify(updatedNotes)).then(
                 (data) => {
-                    console.log("Added");
-                    res.send("Added");
+                    console.log("Note Added");
+                    res.send("Note Added");
                 }
             )
         }
@@ -39,7 +40,26 @@ router.post('/notes', (req, res) => {
 });
 
 
+//route to delete notes
+router.delete('/notes/:id', (req, res) => {
+    let id = req.params.id
+    note.allNotes().then((notes) => {
+        for (const note of notes) {
+            if (note.id === id) {
+                const index = notes.indexOf(note)
+                notes.splice(index, 1)
+                let getAllNotes = JSON.stringify(notes)
+                fs.writeFile('./db/db.json', getAllNotes, (err) =>
+                    err ? console.log(err) : console.log('Note Added'));
 
+
+            }
+
+        }
+
+    })
+    note.allNotes().then((notes) => res.json(notes));
+});
 
 
 
